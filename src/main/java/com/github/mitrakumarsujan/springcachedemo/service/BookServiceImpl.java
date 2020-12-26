8d946e69e72604a6246ada65de633a1ce0d92731
skmitra <mitrakumarsujan@gmail.com>
2020-12-26 13:00:08 +0530

    added log statements

--- src/main/java/com/github/mitrakumarsujan/springcachedemo/service/BookServiceImpl.java
+++ src/main/java/com/github/mitrakumarsujan/springcachedemo/service/BookServiceImpl.java
@@ -4,6 +4,8 @@ import com.github.mitrakumarsujan.springcachedemo.dao.BookDao;
 import com.github.mitrakumarsujan.springcachedemo.exception.BookAlreadyExistsException;
 import com.github.mitrakumarsujan.springcachedemo.exception.BookNotFoundException;
 import com.github.mitrakumarsujan.springcachedemo.model.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,7 @@ import static java.text.MessageFormat.format;
 public class BookServiceImpl implements BookService {
 
     protected BookDao bookDao;
+    private static final Logger LOGGER = LoggerFactory.getLogger(BookServiceImpl.class);
 
     @Autowired
     public BookServiceImpl(BookDao bookDao) {
@@ -42,17 +45,20 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Book getBook(String isbn) throws BookNotFoundException {
+        LOGGER.info("Fetching book with isbn [{}]", isbn);
         Optional<Book> mightHaveBook = bookDao.getBookByIsbn(isbn);
         return mightHaveBook.orElseThrow(() -> getBookNotFoundException(isbn));
     }
 
     @Override
     public List<Book> getBooks() {
+        LOGGER.info("Fetching all books");
         return bookDao.getSavedBooks();
     }
 
     @Override
     public Book updateBook(Book book) throws BookNotFoundException {
+        LOGGER.info("updating book with isbn [{}]",book.getIsbn() );
         Book previousBook = bookDao.updateBook(book);
         if (previousBook == null)
             throw getBookNotFoundException(book.getIsbn());
@@ -61,6 +67,7 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Book deleteBook(String isbn) throws BookNotFoundException {
+        LOGGER.info("deleting book with isbn [{}]",isbn);
         Book deletedBook = bookDao.deleteBookByIsbn(isbn);
         if (deletedBook == null)
             throw getBookNotFoundException(isbn);
