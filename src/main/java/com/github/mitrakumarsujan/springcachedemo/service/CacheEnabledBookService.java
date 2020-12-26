cfd045d6af46b07c159e21c4e2afd85fdc6224cb
skmitra <mitrakumarsujan@gmail.com>
2020-12-26 18:36:32 +0530

    added cache invalidation code

--- src/main/java/com/github/mitrakumarsujan/springcachedemo/service/CacheEnabledBookService.java
+++ src/main/java/com/github/mitrakumarsujan/springcachedemo/service/CacheEnabledBookService.java
@@ -4,8 +4,12 @@ package com.github.mitrakumarsujan.springcachedemo.service;
 import com.github.mitrakumarsujan.springcachedemo.exception.BookAlreadyExistsException;
 import com.github.mitrakumarsujan.springcachedemo.exception.BookNotFoundException;
 import com.github.mitrakumarsujan.springcachedemo.model.Book;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -16,12 +20,17 @@ import java.util.List;
 @Service
 public class CacheEnabledBookService implements BookService {
 
-    private BookService proxyBookService;
     private static final String CACHE_NAME = "books";
+    private static final Logger LOGGER = LoggerFactory.getLogger(CacheEnabledBookService.class);
+    private BookService proxyBookService;
+    private Cache cache;
 
     @Autowired
-    public CacheEnabledBookService(@Qualifier("bookServiceImpl") BookService proxyBookService) {
+    public CacheEnabledBookService(
+            @Qualifier("bookServiceImpl") BookService proxyBookService,
+            CacheManager cacheManager) {
         this.proxyBookService = proxyBookService;
+        this.cache = cacheManager.getCache(CACHE_NAME);
     }
 
     public void setProxyBookService(BookService proxyBookService) {
@@ -46,7 +55,7 @@ public class CacheEnabledBookService implements BookService {
     }
 
     @Override
-    @CacheEvict(cacheNames = CACHE_NAME, key="#book.isbn")
+    @CacheEvict(cacheNames = CACHE_NAME, key = "#book.isbn")
     public Book updateBook(Book book) throws BookNotFoundException {
         return proxyBookService.updateBook(book);
     }
@@ -55,5 +64,16 @@ public class CacheEnabledBookService implements BookService {
     @CacheEvict(cacheNames = CACHE_NAME, key = "#isbn")
     public Book deleteBook(String isbn) throws BookNotFoundException {
         return proxyBookService.deleteBook(isbn);
+    }
+
+    public void invalidateCacheEntries() {
+        LOGGER.info("attempting to clear all cache entries");
+        boolean allInvalidated = cache.invalidate();
+        if(allInvalidated) {
+            LOGGER.info("all cache entries cleared");
+        } else {
+            LOGGER.warn("all cache entries not cleared");
+        }
+
     }
 }
